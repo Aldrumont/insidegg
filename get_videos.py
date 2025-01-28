f6afc7b811829aaa5ab3c250f4bccb8455cb1f80
Junior <ferrazjunior97@gmail.com>
2025-01-28 18:57:21 +0000

    update get_videos.py to download dataset files into a dedicated 'videos' directory

--- get_videos.py
+++ get_videos.py
@@ -2,15 +2,18 @@ import os
 import kaggle
 import shutil
 
-
 # Nome do dataset no Kaggle
 dataset_name = "aldrumont/insidegg-v2"
 
+# Diretório de download
 download_path = "videos"
+
+# Criar a pasta "videos" se não existir
+os.makedirs(download_path, exist_ok=True)
 
 # Baixar o dataset e descompactar
 print(f"Baixando dataset {dataset_name}...")
-kaggle.api.dataset_download_files(dataset_name, path='.', unzip=True)
+kaggle.api.dataset_download_files(dataset_name, path=download_path, unzip=True, quiet=False)
 
 # Mover o arquivo .xlsx para a raiz
 for file in os.listdir(download_path):
